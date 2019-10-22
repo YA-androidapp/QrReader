2940b122607033a8935aa9618d59e0caa9b2a215
YA <YA-androidapp@users.noreply.github.com>
2019-10-22 16:40:57 +0900

    minor updates

--- js/index.js
+++ js/index.js
@@ -50,11 +50,21 @@ function tick() {
       drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF3B58");
       outputMessage.hidden = true;
       outputData.parentElement.hidden = false;
-      outputData.innerText = code.data;
+      outputData.innerText = validURL(code.data) ? "<a href=\"" + code.data + "\" target=\"_blank\">" + code.data + "</a>" : code.data;
       // } else {
       //   outputMessage.hidden = false;
       //   outputData.parentElement.hidden = true;
     }
   }
   requestAnimationFrame(tick);
+}
+
+function validURL(str) {
+  var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+    '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+  return !!pattern.test(str);
 }
